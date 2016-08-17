5aa81a7744fcd57a594bb63c9ee23dec7cfddcf4
carlosvalentindiazFernan <fernancarldiaz@outlook.com>
2016-08-16 23:00:38 -0500

    Add files via upload

---
+++ triangulo.sql
@@ -0,0 +1,12 @@
+
+
+
+select 
+	case
+    WHEN A = B AND B = C then "Equilateral"
+    WHEN (A + B <= C) or(A + C <= B ) or (B + C <= A) then "Not A Triangle"
+    WHEN (A = B AND A != C )  or (A = C AND A != B) or (B = C AND A != B) then "Isosceles"
+    ELSE "Scalene"
+
+	end
+from TRIANGLES;
